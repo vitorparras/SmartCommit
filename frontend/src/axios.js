471a1d99745f58647a0b15cfeea8a25da9369b85
Vitor Parras <vitorhugoparras@hotmail.com>
2024-12-03 02:44:34 -0300

    Base Projeto

---
+++ frontend/src/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080', // URL do backend
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default axiosInstance;
