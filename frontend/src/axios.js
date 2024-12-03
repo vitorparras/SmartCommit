f50d20f57a30f5408908984d296cd767d4762051
Vitor Parras <vitorhugoparras@hotmail.com>
2024-12-03 03:17:23 -0300

    Limpando front

--- frontend/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // URL do backend
-  timeout: 5000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
-export default axiosInstance;
