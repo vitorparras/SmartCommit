35a3820bdbae6b6b608448dace20f9946f2e4477
Vitor Parras <vitorhugoparras@hotmail.com>
2024-12-18 13:34:16 -0300

    `Added functionality to validate Git repository existence and added CommitService struct`

--- backend/internal/adapters/repository/in_memory_repository.go
+++ backend/internal/adapters/repository/in_memory_repository.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"backend/internal/core/domain"
+	"backend/internal/core/ports"
+	"mime/multipart"
+	"os/exec"
 	"sync"
 )
 
@@ -27,4 +30,26 @@ func (r *InMemoryRepository) GetCommits() ([]domain.Commit, error) {
 	r.mutex.RLock()
 	defer r.mutex.RUnlock()
 	return r.commits, nil
+}
+
+type CommitService struct {
+	ai   ports.AIPort
+	repo ports.RepositoryPort
+}
+
+// Método para validar a existência do repositório Git
+func (s *CommitService) ValidateGitRepository(readmeFile *multipart.FileHeader) (bool, error) {
+	// Verifica se o arquivo README existe
+	if readmeFile == nil {
+		return false, nil
+	}
+
+	// Verifica se o arquivo README está em um repositório Git
+	cmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
+	err := cmd.Run()
+	if err != nil {
+		return false, err // Não é um repositório Git
+	}
+
+	return true, nil // É um repositório Git
 }
